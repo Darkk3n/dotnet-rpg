63c57e39f87cbba897d8e3c8c8d1e3d1c128662f
Gerardo <gerardo.aguilar01@outlook.com>
2021-03-27 11:07:41 -0600

    Implemented relation between Users and Characters

--- Models/Character.cs
+++ Models/Character.cs
@@ -9,5 +9,7 @@ namespace dotnet_rpg.Models
         public int Defense { get; set; }
         public int Intelligence { get; set; } = 10;
         public RpgClass Class { get; set; } = RpgClass.Knight;
+
+        public User User { get; set; }
     }
 }
