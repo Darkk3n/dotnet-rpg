24f50eb1c22ed5f2bcf251ffb00b84d96f76aed3
Gerardo Aguilar <gerardoaguilar@mtech-systems.com>
2021-03-27 22:56:22 -0600

    Implemented Many to Many relationship

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -14,5 +14,14 @@ namespace dotnet_rpg.Data
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<CharacterSkill> CharacterSkills { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+            .Entity<CharacterSkill>()
+            .HasKey(r => new { r.CharacterId, r.SkillId });
+        }
     }
 }
